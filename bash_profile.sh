fd5387393f4712881cbf993834afa93fe58c79c2
Reilly Forshaw <reilly.forshaw@gmail.com>
2018-04-04 16:29:43 -0700

    no_place_like_home

---
+++ bash_profile.sh
@@ -0,0 +1,4 @@
+export PS1="\[\e[0;37m\][\t]\[\e[0m\] \[\e[0;36m\]\u\[\e[0m\]:\[\e[0;95m\]\W\[\e[0m\] $ "
+export PS2=" $ "
+
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
